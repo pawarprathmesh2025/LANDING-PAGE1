a6242d23e40cacb265e2e8452477cf30e03b5cd5
amruta <sobaleamruta@gmail.com>
2026-01-21 18:10:15 +0530

    changed contact layout.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";  // fixed path
-
+import Contact from "./components/Contact";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
         
       </Routes>
     </Router>
---
+++ src/components/Contact.jsx
@@ -0,0 +1,18 @@
+// import ContactPage from "../components/ContactPage";
+// import ContactForm from "../components/ContactForm";
+import DownloadFooter from "../components/DownloadFooter";
+import ContactPg from "../components/ContactPg";
+
+const Contact = () => {
+  return (
+    <>
+      {/* <ContactPage />
+        <ContactForm /> */}
+         <ContactPg/>
+      <DownloadFooter />
+     
+    </>
+  );
+};
+
+export default Contact;
